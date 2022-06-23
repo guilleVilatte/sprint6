0bd7fba8cf83cda4335d3cd8e291a809b2044e38
Guille <guillevilatte@gmail.com>
2022-06-23 10:54:09 +0200

    nivel2completo

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,58 +1,77 @@
 <template>
   <div>
-    <div class="botones">
-      <BotonS :text="'Siguiente'" @click1="next" @click2="before"></BotonS>
+    <PresentacionP class="comienzo" @click="iniciar"></PresentacionP>
+    <div v-if="start === true">
+      <div class="botones">
+        <BotonS @click1="next" @click2="before"></BotonS>
+      </div>
+      <EscenaA
+        class="proyecto"
+        :frases="postFrases"
+        :currentSentence="postCurrentSentence"
+        @click1="next"
+        @click2="before"
+      ></EscenaA>
     </div>
-    <EscenaA :frases="postFrases" :currentSentence="postCurrentSentence" @click1="next" @click2="before"></EscenaA>
   </div>
 </template>
 
 <script>
 import EscenaA from "./EscenaA.vue";
 import BotonS from "./BotonS.vue";
-
+import PresentacionP from "./PresentacionP.vue";
 
 export default {
   name: "HomePage",
   components: {
     EscenaA,
     BotonS,
-  }, data(){
-    return{
-      postFrases:[
-        'Nuestro héroe estaba flotando por el espacio sideral cuando a lo lejos divisó una nave espacial',
-        'Sentía curiosidad por el interior de la nave y se puso a inspeccionarla. Llegó a una sala con dos puertas.',
-        'El héroe decidió atravesar la puerta que le llevaba a casa',
-        'Mientras tanto, otros héroes no tuvieron tanta suerte en su elección...',
+    PresentacionP,
+  },
+  data() {
+    return {
+      postFrases: [
+        "Nuestro héroe estaba flotando por el espacio sideral cuando a lo lejos divisó una nave espacial",
+        "Sentía curiosidad por el interior de la nave y se puso a inspeccionarla. Llegó a una sala con dos puertas.",
+        "El héroe decidió atravesar la puerta que le llevaba a casa",
+        "Mientras tanto, otros héroes no tuvieron tanta suerte en su elección...",
       ],
-      postCurrentSentence: null
-    }
+      postCurrentSentence: null,
+      start: false,
+    };
   },
   methods: {
     next() {
-      if(this.postCurrentSentence<3){
-        document.querySelectorAll(".caja")[0].style.backgroundColor="white"
-        this.postCurrentSentence++
+      if (this.postCurrentSentence < 3) {
+        document.querySelectorAll(".caja")[0].style.backgroundColor = "white";
+        this.postCurrentSentence++;
       } else {
-        this.postCurrentSentence=0
-        document.querySelectorAll(".caja")[0].style.backgroundColor="pink"
+        this.postCurrentSentence = 0;
+        document.querySelectorAll(".caja")[0].style.backgroundColor = "pink";
       }
     },
     before() {
-      if(this.postCurrentSentence>0){
-        this.postCurrentSentence--
-        document.querySelectorAll(".caja")[0].style.backgroundColor="white"
+      if (this.postCurrentSentence > 0) {
+        this.postCurrentSentence--;
+        document.querySelectorAll(".caja")[0].style.backgroundColor = "white";
       } else {
-        this.postCurrentSentence=3
-        document.querySelectorAll(".caja")[0].style.backgroundColor="white"
-      } 
-      if(this.postCurrentSentence==0){
-        document.querySelectorAll(".caja")[0].style.backgroundColor="pink"
+        this.postCurrentSentence = 3;
+        document.querySelectorAll(".caja")[0].style.backgroundColor = "white";
+      }
+      if (this.postCurrentSentence == 0) {
+        document.querySelectorAll(".caja")[0].style.backgroundColor = "pink";
       }
     },
-  }
-}
-
+    iniciar() {
+      if(this.start==false){
+        this.start = true;
+        this.postCurrentSentence = 0
+      } else {
+        this.start = false
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
---
+++ src/components/PresentacionP.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="presentacion">
+    <h1>Bienvenido al pryecto numero uno con Vue</h1>
+    <button @click="$emit('iniciar')">Iniciar</button>
+</div>
+</template>
+
+<script>
+export default{
+    name: 'PresentacionP'
+}
+
+</script>
+
+<style>
+    .presentacion{
+        text-align: center;
+        margin-bottom: 50px;
+    }
+</style>
